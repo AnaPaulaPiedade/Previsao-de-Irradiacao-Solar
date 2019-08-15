2af46b485fae3d3980e41be1d56255f115a5fb4f
ANA PAULA PIEDADE <ana-paula2308@hotmail.com>
2019-08-15 16:54:10 -0300

    Leitura e junção dos CSV's de um diretorio. Filtragem intervalo de horas.

---
+++ dados_Preparados/preparacao_Dados.R
@@ -0,0 +1,59 @@
+# Indica diretório que vai trabalhar
+setwd('C:\\Users\\ana-p\\Desktop\\dados')
+
+# lista todos os arquivos com extensão .csv
+input <- dir(pattern='.csv')
+L <- length(input)
+
+
+# Lendo arquivos e salvando em uma lista
+dados <- NULL
+for (i in 1:L){
+  dados[[i]] <- read.csv(input[i],h=T, sep = ",")
+  cat(input[i],'\n')
+  #print(dados[i]);
+}
+
+# Juntando todos os CSV's em um só
+var_Data <- list();
+var_Hora <- list();
+data_completa <- do.call("rbind",dados);
+
+#Modificando data e hora para substituir
+time <- as.character(data_completa$Date.time);
+for(i in 1:length(time)){
+  #print(strsplit(time[i]," ")[[1]][2]);
+  var_Data[i] <- strsplit(time[i]," ")[[1]][1];
+  var_Hora[i] <- strsplit(time[i]," ")[[1]][2];
+}
+
+#print(var_Data[274]);
+#print(var_Hora[274]);
+
+
+# Exclui Date/Time do dataframe
+data_completa$Date.time <- NULL;
+
+#Modificando o dataframe
+data_completa$Data <- var_Data;
+data_completa$Hora <- var_Hora;
+print(data_completa);
+
+data_completa$Data <- as.character(data_completa$Data);
+data_completa$Hora <- as.character(data_completa$Hora);
+
+
+#Filtro
+
+
+comp <- data_completa[(data_completa$Hora >= "06:00:00" & data_completa$Hora <= "18:00:00" ), ]
+print(comp);
+
+comp <- as.matrix(comp);
+write.csv(comp, "joints/teste.csv", row.names = FALSE);
+  
+
+
+### Cria diretório
+dir.create('~/caminho/')
+
